2c79906e8478e11b245260d6348422371ba5f303
zach ryan <zachryan@pop-os.localdomain>
2020-04-29 10:28:11 -0600

    added all test predictions

--- task1/openNMT_driver.sh
+++ task1/openNMT_driver.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #lang_code=$1
-all_langs=("ice" "jpn" "kor" "lit" "rum" "vie")
+#all_langs=("ady" "arm" "bul" "dut" "fre" "geo" "gre" "hin" "hun" "ice" "jpn" "kor" "lit" "rum" "vie")
+all_langs=("fre" "kor")
 for lang_code in ${all_langs[@]};
 do
   src_train="./model_${lang_code}_lstm/${lang_code}_train_src.txt"
@@ -10,28 +11,31 @@ do
   src_dev="./model_${lang_code}_lstm/${lang_code}_dev_src.txt"
   tgt_dev="./model_${lang_code}_lstm/${lang_code}_dev_tgt.txt"
   echo $src_dev
-  #src_test="./model_${lang_code}_lstm/${lang_code}_test_src.txt"
+  src_test="./model_${lang_code}_lstm/${lang_code}_test_src.txt"
   #tgt_test="./model_${lang_code}_lstm/${lang_code}_test_tgt.txt"
 
   preprocess="./model_${lang_code}_lstm/${lang_code}_preprocess.low"
 
   model="./model_${lang_code}_lstm/${lang_code}_model_lstm"
-  #model_translate="./model_${lang_code}_lstm/${model}_step_50000.pt"
-
-  #predicted="./model_${lang_code}_lstm/${model}_step_50000_pred"
+  model_translate="${model}_step_50000.pt"
+  echo $model_translate
+  predicted="${model}_step_50000_pred_10_best"
 
   #uncomment if language needs to format for openNMT
   #python transform_data.py $lang_code
 
   #wait
 
-  onmt_preprocess -train_src $src_train -train_tgt $tgt_train -valid_src $src_dev -valid_tgt $tgt_dev -save_data $preprocess -lower
+  #onmt_preprocess -train_src $src_train -train_tgt $tgt_train -valid_src $src_dev -valid_tgt $tgt_dev -save_data $preprocess -lower
 
-  wait
+  #wait
 
-  onmt_train -data $preprocess -save_model $model -gpu_ranks 0 -train_steps 50000
+  #onmt_train -data $preprocess -save_model $model -gpu_ranks 0 -train_steps 50000
 
-  wait
+  #wait
 
   #onmt_translate -gpu 0 -model $model_translate -src $src_test -tgt $tgt_test -replace_unk -verbose -output $predicted
+  #onmt_translate -gpu 0 -model $model_translate -src $src_test -replace_unk -verbose -n_best 10 -output $predicted
+
+  #wait
 done
--- task1/transform_data.py
+++ task1/transform_data.py
@@ -26,7 +26,7 @@ def extract_train():
     f_tgt.close()
 
 #for dev data
-def extract_dev_and_test():
+def extract_dev():
     switch = 0
 
     for temp_lang in ALL_LANGUAGE_ABBREVIATIONS:
@@ -51,5 +51,32 @@ def extract_dev_and_test():
     f_dev_tgt.close()
 
 
-extract_train()
-extract_dev_and_test()
+#for dev data
+def extract_test():
+    switch = 0
+
+    for temp_lang in ALL_LANGUAGE_ABBREVIATIONS:
+        f_test_src = open("model_"+temp_lang+"_lstm/"+temp_lang+"_test_src.txt", "w")
+        #f_test_tgt = open("model_"+temp_lang+"_lstm/"+temp_lang+"_test_tgt.txt", "w")
+
+
+        with open("data/test/" + temp_lang + "_test.txt") as txtfile:
+            reader = csv.reader(txtfile)
+            for row in reader:
+                temp_transform_grapheme = temp_lang + " "
+                for ch in row[0]:
+                    temp_transform_grapheme = temp_transform_grapheme + ch + " "
+                temp_transform_grapheme += temp_lang
+                #temp_transform_phoneme = temp_lang + " " + row[1] + " " + temp_lang
+
+                f_test_src.write(temp_transform_grapheme + "\n")
+                #f_test_tgt.write(temp_transform_phoneme + "\n")
+
+
+    f_test_src.close()
+    #f_test_tgt.close()
+
+
+#extract_train()
+#extract_dev()
+extract_test()
