a0c42176e6996b15eb534714f05d428a57bb87ee
zach ryan <zachryan@pop-os.localdomain>
2020-04-29 03:01:53 -0600

    added gre, hin, hun, ice, jpn, kor, lit, rum, vie models

--- task1/openNMT_driver.sh
+++ task1/openNMT_driver.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
-lang_code=$1
-src_train="./model_${lang_code}_lstm/${lang_code}_train_src.txt"
-tgt_train="./model_${lang_code}_lstm/${lang_code}_train_tgt.txt"
+#lang_code=$1
+all_langs=("ice" "jpn" "kor" "lit" "rum" "vie")
+for lang_code in ${all_langs[@]};
+do
+  src_train="./model_${lang_code}_lstm/${lang_code}_train_src.txt"
+  tgt_train="./model_${lang_code}_lstm/${lang_code}_train_tgt.txt"
+  echo $src_train
+  src_dev="./model_${lang_code}_lstm/${lang_code}_dev_src.txt"
+  tgt_dev="./model_${lang_code}_lstm/${lang_code}_dev_tgt.txt"
+  echo $src_dev
+  #src_test="./model_${lang_code}_lstm/${lang_code}_test_src.txt"
+  #tgt_test="./model_${lang_code}_lstm/${lang_code}_test_tgt.txt"
 
-src_dev="./model_${lang_code}_lstm/${lang_code}_dev_src.txt"
-tgt_dev="./model_${lang_code}_lstm/${lang_code}_dev_tgt.txt"
+  preprocess="./model_${lang_code}_lstm/${lang_code}_preprocess.low"
 
-#src_test="./model_${lang_code}_lstm/${lang_code}_test_src.txt"
-#tgt_test="./model_${lang_code}_lstm/${lang_code}_test_tgt.txt"
+  model="./model_${lang_code}_lstm/${lang_code}_model_lstm"
+  #model_translate="./model_${lang_code}_lstm/${model}_step_50000.pt"
 
-preprocess="./model_${lang_code}_lstm/${lang_code}_preprocess.low"
+  #predicted="./model_${lang_code}_lstm/${model}_step_50000_pred"
 
-model="./model_${lang_code}_lstm/${lang_code}_model_lstm"
-#model_translate="./model_${lang_code}_lstm/${model}_step_50000.pt"
+  #uncomment if language needs to format for openNMT
+  #python transform_data.py $lang_code
 
-#predicted="./model_${lang_code}_lstm/${model}_step_50000_pred"
+  #wait
 
-#uncomment if language needs to format for openNMT
-#python transform_data.py $lang_code
+  onmt_preprocess -train_src $src_train -train_tgt $tgt_train -valid_src $src_dev -valid_tgt $tgt_dev -save_data $preprocess -lower
 
-#wait
+  wait
 
-onmt_preprocess -train_src $src_train -train_tgt $tgt_train -valid_src $src_dev -valid_tgt $tgt_dev -save_data $preprocess -lower
+  onmt_train -data $preprocess -save_model $model -gpu_ranks 0 -train_steps 50000
 
-wait
+  wait
 
-onmt_train -data $preprocess -save_model $model -gpu_ranks 0 -train_steps 50000
-
-#wait
-
-#onmt_translate -gpu 0 -model $model_translate -src $src_test -tgt $tgt_test -replace_unk -verbose -output $predicted
+  #onmt_translate -gpu 0 -model $model_translate -src $src_test -tgt $tgt_test -replace_unk -verbose -output $predicted
+done
